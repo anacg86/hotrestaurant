b0c7a4c2629ef5784a3bdc3bbd05ac04d8a8a5db
Callanthas <lord_of_light1@hotmail.com>
2019-01-12 13:18:01 -0600

    added visitor counter in the console

--- restaurant.js
+++ restaurant.js
@@ -3,6 +3,10 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+var visitors = [
+  {visit: 0}
+  ];
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,14 +20,20 @@ var waitlist = [];
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
+  visitors[0].visit ++;
+  console.log(visitors[0]);
 });
 
 app.get("/reservations", function (req, res) {
   res.sendFile(path.join(__dirname, "make_reservation.html"));
+  visitors[0].visit ++;
+  console.log(visitors[0]);
 });
 
 app.get("/tables", function (req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
+  visitors[0].visit ++;
+  console.log(visitors[0]);
 });
 // Displays all reservations
 app.get("/api/reservations", function (req, res) {
@@ -42,11 +52,15 @@ app.post("/api/tables", function (req, res) {
     newreservation.routeName = newreservation.uniqueID.replace(/\s+/g, "").toLowerCase();
     console.log(newreservation);
     waitlist.push(newreservation);
+    visitors[0].visit ++;
+    console.log(visitors[0]);
   }
   else {
     newreservation.routeName = newreservation.uniqueID.replace(/\s+/g, "").toLowerCase();
     console.log(newreservation);
     reservations.push(newreservation);
+    visitors[0].visit ++;
+    console.log(visitors[0]);
   }
   //manda en formato jason
    res.json(newreservation);
@@ -57,4 +71,3 @@ app.post("/api/tables", function (req, res) {
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
-
